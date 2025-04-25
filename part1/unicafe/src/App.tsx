fce1927bfa68d2d95c3acf17be47ef7eed5aa578
Kilian <norxen.gamedeveloper@gmail.com>
2025-04-25 08:50:06 +0200

    Activity 1.7 unicafe

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -5,6 +5,11 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  // Functions to calculate average, all and positive
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -16,6 +21,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
