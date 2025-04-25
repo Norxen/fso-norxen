f02e64958f5e4cdb2f6be5d75b0afcef4d1dab2b
Kilian <norxen.gamedeveloper@gmail.com>
2025-04-25 14:48:44 +0200

    Abstracting the Button into a component

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import Statistics from './components/Statistics'
 
+// Define the button component
+const Button = (props:{ text:string, onClick: () => void }) => {
+  return <button onClick={props.onClick}>{props.text}</button>
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -11,9 +16,9 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button text="good" onClick={() => setGood(good + 1)} />
+      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" onClick={() => setBad(bad + 1)} />
 
       {hasFeedback() ? (
         <Statistics good={good} neutral={neutral} bad={bad} />
