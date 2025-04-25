2018979cbc85a82eca65ffcf87000248fa084855
Kilian <norxen.gamedeveloper@gmail.com>
2025-04-25 08:55:25 +0200

    Activity 1.8

---
+++ part1/unicafe/src/components/Statistics.tsx
@@ -0,0 +1,21 @@
+const Statistics = (props: {good: number, neutral: number, bad: number}) => {
+    // Functions to calculate average, all and positive
+    const all = props.good + props.neutral + props.bad
+    const average = (props.good - props.bad) / all
+    const positive = (props.good / all) * 100
+
+    return (
+        <div>
+            <h1>statistics</h1>
+            <p>good {props.good}</p>
+            <p>neutral {props.neutral}</p>
+            <p>bad {props.bad}</p>
+
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {positive} %</p>
+        </div>
+    ) 
+}
+
+export default Statistics
