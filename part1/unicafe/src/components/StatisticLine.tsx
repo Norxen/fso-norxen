4195c016a0f40e6406b34c563d8bf6df486a5e39
Kilian <norxen.gamedeveloper@gmail.com>
2025-04-25 09:07:31 +0200

    Activity 1.11

--- part1/unicafe/src/components/StatisticLine.tsx
+++ part1/unicafe/src/components/StatisticLine.tsx
@@ -1,12 +1,21 @@
 type StatisticLineProps = {
-    text: string
-    value: number
-}
+    text: string;
+    value: number;
+};
 
-const StatisticLine = (props: StatisticLineProps) => (
-    <p>
-        {props.text} {props.value} {props.text === 'positive' ? ' %' : ''}
-    </p>
-)
+const StatisticLine = (props: StatisticLineProps) => {
+    const displayValue =
+        props.value % 1 !== 0 ? props.value.toFixed(1) : props.value;
 
-export default StatisticLine
+    return (
+        <tr>
+            <td>{props.text}</td>
+            <td>
+                {displayValue}
+                {props.text === 'positive' ? ' %' : ''}
+            </td>
+        </tr>
+    );
+};
+
+export default StatisticLine;
--- part1/unicafe/src/components/Statistics.tsx
+++ part1/unicafe/src/components/Statistics.tsx
@@ -1,21 +1,25 @@
 import StatisticLine from "./StatisticLine";
 
 const Statistics = (props: { good: number; neutral: number; bad: number }) => {
-    const all = props.good + props.neutral + props.bad
-    const average = all === 0 ? 0 : (props.good - props.bad) / all
-    const positive = all === 0 ? 0 : (props.good / all) * 100
+    const all = props.good + props.neutral + props.bad;
+    const average = all === 0 ? 0 : (props.good - props.bad) / all;
+    const positive = all === 0 ? 0 : (props.good / all) * 100;
 
     return (
         <div>
             <h1>statistics</h1>
-            <StatisticLine text="good" value={props.good} />
-            <StatisticLine text="neutral" value={props.neutral} />
-            <StatisticLine text="bad" value={props.bad} />
-            <StatisticLine text="all" value={all} />
-            <StatisticLine text="average" value={average} />
-            <StatisticLine text="positive" value={positive} />
+            <table>
+                <tbody>
+                    <StatisticLine text="good" value={props.good} />
+                    <StatisticLine text="neutral" value={props.neutral} />
+                    <StatisticLine text="bad" value={props.bad} />
+                    <StatisticLine text="all" value={all} />
+                    <StatisticLine text="average" value={average} />
+                    <StatisticLine text="positive" value={positive} />
+                </tbody>
+            </table>
         </div>
-    )
-}
+    );
+};
 
-export default Statistics
+export default Statistics;
